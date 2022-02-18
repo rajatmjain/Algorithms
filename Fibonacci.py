0241a6ccce4925241fa9f5567be904fdaeae93ad
Rajat Jain <rajat.apple007@gmail.com>
2022-02-17 21:53:11 -0800

    Dynamic Programming with and without seeds

--- Fibonacci.py
+++ Fibonacci.py
@@ -5,3 +5,19 @@ def fiboRecur(n):
         return fiboRecur(n-1) + fiboRecur(n-2)
 
 print(fiboRecur(10))
+
+def fiboDyn(n):
+    f = [0,1]
+    for i in range(2,n+1):
+        f.append(f[i-1]+f[i-2])
+    return f[n]
+
+print(fiboDyn(5))
+
+def fiboDynSeed(n,seed1,seed2):
+    f = [seed1,seed2]
+    for i in range(2,n+1):
+        f.append(f[i-1]+f[i-2])
+    return f[n]
+
+print(fiboDynSeed(5,3,7))
