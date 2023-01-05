64007963e2225531f7a6f66914610eebd34bc843
rjain032 <rajat.jain@email.ucr.edu>
2023-01-04 22:44:06 -0800

    Final

--- DepthFirstSearchIterative.py
+++ DepthFirstSearchIterative.py
@@ -20,24 +20,23 @@ def dfs(root: Node):
     
     return visitedOrder
 
-# a = Node('a')
-# b = Node('b')
-# c = Node('c')
-# d = Node('d')
-# e = Node('e')
-# f = Node('f')
-# g = Node('g')
-# h = Node('h')
-
-# a.left = b
-# a.right = c
-# b.left = d
-# b.right = e
-# c.left = f
-# c.right = g
-# d.left = h
-
-a = Node(None)
+# Nodes creation
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+# Tree creation
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+c.right = g
+d.left = h
 
 print("Depth First Search")
 print("Visited in order: ",dfs(a))
--- DepthFirstSearchRecursive.py
+++ DepthFirstSearchRecursive.py
@@ -14,10 +14,8 @@ def dfs(root: Node):
     dfs(root.right)
 
     return visitedOrder
-
-  
-  
-
+ 
+# Nodes creation
 a = Node('a')
 b = Node('b')
 c = Node('c')
@@ -26,7 +24,7 @@ e = Node('e')
 f = Node('f')
 g = Node('g')
 h = Node('h')
-
+# Tree creation
 a.left = b
 a.right = c
 b.left = d
