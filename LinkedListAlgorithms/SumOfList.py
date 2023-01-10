66287274f871068eb6671fd2593b9089eed1c842
rjain032 <rajat.jain@email.ucr.edu>
2023-01-10 15:13:30 -0800

    Sum of list

---
+++ LinkedListAlgorithms/SumOfList.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,value) -> None:
+        self.value = value
+        self.next = None
+
+
+def traversalIter(head:Node):
+    sum = 0
+    while(head!=None):
+        sum += head.value
+        head = head.next
+    return sum
+
+def traversalRec(head:Node):
+    if(head==None):
+        return 0
+    return head.value + traversalRec(head.next)
+    
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+print("Sum of list Iter: ",traversalIter(a))
+print("Sum of list Recur: ",traversalRec(a))
