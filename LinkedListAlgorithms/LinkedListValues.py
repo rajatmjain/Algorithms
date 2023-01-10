7c72c37d84a2477742610be69af14d3f28357414
rjain032 <rajat.jain@email.ucr.edu>
2023-01-10 15:08:30 -0800

    Values problem

---
+++ LinkedListAlgorithms/LinkedListValues.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,value) -> None:
+        self.value = value
+        self.next = None
+
+
+def traversalIter(head:Node):
+    values = []
+    while(head!=None):
+        values.append(head.value)
+        head = head.next
+    return values
+
+def traversalRec(head:Node,values:list):
+    if(head==None):
+        return
+    values.append(head.value)
+    if(head.next!=None):
+        traversalRec(head.next,values)
+    return values
+    
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+print("Values Iter: ",traversalIter(a))
+print("Values Rec: ",traversalRec(a,[]))
